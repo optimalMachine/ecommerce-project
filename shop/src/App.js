9bd97164599a6f28db56b71fce4c627f01ee244e
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-07-05 20:44:05 -0500

    updates App.js
    adding shoes data

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from "react";
+import shoedata from "./data.js";
 
 function App() {
+
+  let [shoes,setShoes] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home"><span><i class="bi bi-shop"></i></span> ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+      <div class="main-bg"></div>
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img className = "front-shoes" src={process.env.PUBLIC_URL + '/images/shoes1.jpg'} /> 
+            <h4>Product No.1</h4>
+            <p>Explanation</p>
+          </div>
+          <div className="col-md-4">
+            <img className = "front-shoes" src={process.env.PUBLIC_URL + '/images/shoes2.jpg'} /> 
+            <h4>Product No.2</h4>
+            <p>Explanation</p>
+          </div>
+          <div className="col-md-4">
+            <img className = "front-shoes" src={process.env.PUBLIC_URL + '/images/shoes3.jpg'} /> 
+            <h4>Product No.3</h4>
+            <p>Explanation</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
