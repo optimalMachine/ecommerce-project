c5632355a371d5cf377c18f6ee77cea6cce75ae8
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-07-08 20:49:16 -0500

    Update App.js
    creates Card component

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from "react";
@@ -16,8 +15,7 @@ function App() {
           <Navbar.Brand href="#home"><span><i class="bi bi-shop"></i></span> ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -27,32 +25,24 @@ function App() {
       <div className="container">
         <div className="row">
           {
-            <FrontShoes shoedata={shoedata}/>
+            shoes.map((data,i)=>{
+              return(
+                <Card i={i} shoedata={shoedata}/>
+              )
+            })
           }
-
-          <div className="col-md-4">
-            <img className = "front-shoes" src={process.env.PUBLIC_URL + '/images/shoes2.jpg'} /> 
-            <h4>{shoedata[1].title}</h4>
-            <p>Explanation</p>
-          </div>
-          <div className="col-md-4">
-            <img className = "front-shoes" src={process.env.PUBLIC_URL + '/images/shoes3.jpg'} /> 
-            <h4>{shoedata[2].title}</h4>
-            <p>Explanation</p>
-          </div>
         </div>
       </div>
-
     </div>
-    
   );
 
-  function FrontShoes(props){
+  function Card(props){
     return(
       <div className="col-md-4">
-        <img className = "front-shoes" src={process.env.PUBLIC_URL + './images/shoes1.jpg'} /> 
-        <h4>{props.shoedata[0].title}</h4>
-        <p>Explanation</p>
+        <img className = "front-shoes" src={process.env.PUBLIC_URL + './images/shoes'+[props.i+1]+'.jpg'} /> 
+        <h4>{props.shoedata[props.i].title}</h4>
+        <p>{props.shoedata[props.i].content}</p>
+        <p>price: {props.shoedata[props.i].price} Won(KRW)</p>
       </div>
     );
   };
